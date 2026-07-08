7c6ca37501c1282ab9ca9a3bc5d3b746c15defb0
dayanandaj055 <dayanandaj055@gmail.com>
2026-07-08 11:40:30 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 print("Welcome to the Collaborative Calculator!")
 print("Addition of 5 and 3 is:", add(5, 3))
+print("Multiplication of 5 and 3 is:", multiply(5, 3))
